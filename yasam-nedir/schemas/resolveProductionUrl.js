200ee1915f14490a8d1d12d287aa8dd5e1323718
Koray Aydemir <korayaydemir276@gmail.com>
2022-08-16 04:24:35 +0300

    added half complete preview for sanity

--- yasam-nedir/schemas/resolveProductionUrl.js
+++ yasam-nedir/schemas/resolveProductionUrl.js
@@ -1,3 +1,36 @@
+import sanityClient from "../../src/Client";
+function useGetData(dataa) {
+  //    sanityClient
+  //   .fetch(
+  //     `*[_type == "Bolum" ]{"birim_no": related_birim-> birim_no }` /*   `*[_type == "Bolum" ]{bolum_no, title, "birim_title": related_birim->title}`*/
+  //   )
+  //   .then((data) => {
+  //     dataa = data;
+  //   })
+  //   .catch(console.error);
+  // return dataa;
+  //
+}
 export default function resolveProductionUrl(document) {
-  return `https://my-site.com/posts/${document.slug.current}`;
+  const query = `*[_id == "${document._id}"]{'birim_no': related_birim-> birim_no}`;
+  async function fetch() {
+    window.myData = await sanityClient.fetch(query);
+  }
+  fetch();
+  if (window.myData && window.myData[0]) {
+    if (document._type === "Bolum") {
+      return `https://yasamnedir.web.app/yasambilim/birim-${window.myData[0].birim_no}/bolum-${document.bolum_no}`;
+    }
+  }
+
+  // const theData = useGetData();
+  // let dat = ";";
+  // console.log(useGetData(dat));
+  //
+
+  if (document._type === "Birim") {
+    return `https://yasamnedir.web.app/yasambilim/${document._type.toLowerCase()}-${
+      document.birim_no
+    }`;
+  }
 }
