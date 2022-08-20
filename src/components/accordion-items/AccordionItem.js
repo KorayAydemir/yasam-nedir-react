cbe316806cad9cd70d3d3b5a7565d08911d4496a
Koray Aydemir <korayaydemir276@gmail.com>
2022-08-20 16:33:20 +0300

    removed comment

--- src/components/accordion-items/AccordionItem.js
+++ src/components/accordion-items/AccordionItem.js
@@ -5,13 +5,13 @@ import imageUrlBuilder from "@sanity/image-url";
 import classes from "./AccordionItem.module.css";
 
 const AccordionItem = (props) => {
-  console.log("h");
+  //console.log("h");
   const [isActive, setIsActive] = useState(false);
   const builder = imageUrlBuilder(sanityClient);
   function urlFor(source) {
     return builder.image(source);
   }
-  console.log(props.value);
+  //  console.log(props.value);
   const title = props.title;
   const serializer = {
     types: {
@@ -55,90 +55,6 @@ const AccordionItem = (props) => {
     },
   };
 
-  /*const [theHtml, setTheHtml] = useState();
-	useEffect(() => {
-	  setTheHtml(
-		document
-		  .getElementsByClassName("texts")[0]
-		  .innerHTML.replace(
-			new RegExp("Elimizde", "gm"),
-			"<span>" + "Elimizde" + "</span>"
-		  )
-	  );
-	}, []);
-	console.log(theHtml);*/
-  // const [words, setWords] = useState();
-  // const [baseHtml, setBaseHtml] = useState();
-  //useEffect(() => {
-  //  sanityClient
-  //  .fetch(`*[_type == "tooltips"]`)
-  // .then((data) => setWords(data[0].kelimeler.slice(0, 10)))
-  // .catch(console.error);
-
-  //setBaseHtml(document.getElementsByClassName("texts")[1].innerHTML);
-  /*for (const key of document.getElementsByClassName("texts")) {
-		setBaseHtml(key.innerHTML);
-	  }*/
-
-  /* document.getElementsByClassName("new")[0].innerHTML = document
-		.getElementsByClassName("texts")[0]
-		.innerHTML.replace(
-		  new RegExp("Elimizde", "gm"),
-		  "<span class='tooltip' data-text='Thank for hovering! I'm a tooltip.'>" +
-			"Elimizde" +
-			"</span>"
-		);*/
-  /* for (const key of document.getElementsByClassName("texts")) {
-		key.hidden = true;
-	  }*/
-  // }, []);
-
-  //WHAT THE FUCK DID I JUST CODE AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-  /* if (words) {
-	  let k = 0;
-	  for (const key of document.getElementsByClassName("texts")) {
-		let b = 0;
-  
-		words.map((word) => {
-		  word = word.split("=");
-		  if (b === 0) {
-			document.getElementsByClassName("new")[k].innerHTML =
-			  key.innerHTML.replace(
-				new RegExp("\\b" + word[0] + "\\b", "gm"),
-				`<span class='tooltip' data-text='${word[1]}'>` +
-				  word[0] +
-				  "</span>"
-			  );
-			b = b + 1;
-		  } else {
-			document.getElementsByClassName("new")[k].innerHTML = document
-			  .getElementsByClassName("new")
-			  [k].innerHTML.replace(
-				new RegExp("\\b" + word[0] + "\\b", "gm"),
-				`<span class='tooltip' data-text='${word[1]}'>` +
-				  word[0] +
-				  "</span>"
-			  );
-		  }
-		});
-  
-		k = k + 1;
-	  }
-	}*/
-  /*a = a.replace(
-		  new RegExp(word[0], "gm"),
-		  `<span class='tooltip' data-text='${word[1]}'>` + word[0] + "</span>"
-		);*/ //REACTIVate THIS
-  //  console.log(a);
-  // document.getElementsByClassName("new")[1].innerHTML = a; //REACTIVate THIS
-
-  /*  document.getElementsByClassName("new")[0].innerHTML = document
-		  .getElementsByClassName("texts")[0]
-		  .innerHTML.replace(
-			new RegExp(word[0], "gm"),
-			`<span class='tooltip' data-text='${word[1]}'>` + word[0] + "</span>"
-		  );*/
-
   return (
     <div className={classes.wrapper}>
       <span
@@ -157,7 +73,7 @@ const AccordionItem = (props) => {
             : `${classes.inactive} ${classes.unset}`
         }
       >
-        <div className="new"></div>
+        <div className="new" style={{ marginTop: "10px" }}></div>
 
         <div className="texts">
           <PortableText value={props.value} components={serializer} />
