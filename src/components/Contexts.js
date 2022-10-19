8457ee230381a7b93844ffed685d167858fea2a7
Koray Aydemir <korayaydemir276@gmail.com>
2022-10-20 00:34:06 +0300

    ab

--- src/components/Contexts.js
+++ src/components/Contexts.js
@@ -1,28 +1,47 @@
 import { useState, createContext, useEffect, useReducer } from 'react';
 import sanityClient from "../Client"
 
-export const DataContext = createContext()
+export const DataContext = createContext(null)
 export const DataDispatchContext = createContext(null);
 
 
-function dataReducer(state, action) {
-  switch (action.type) {
-    case 'birim-bolum': {
-      return ["a"]
-    }
-    case 'alt-bolum': {
-      return action
-    }
-    default: {
-      throw Error('Unknown action: ' + action.type);
+export function DataProvider({ children }) {
+  const [data, dispatch] = useReducer(dataReducer, initialData)
+  const [birimler, setBirimler] = useState(null)
+  function dataReducer(data, action) {
+    switch (action.type) {
+      case 'birim-bolum': {
+        return ["a"]
+      }
+      case 'alt-bolum': {
+        return action
+      }
+      case 'denemeler': {
+        return { obj: "abc" }
+      }
+      default: {
+        throw Error('Unknown action: ' + action.type);
+      }
     }
   }
-}
-
-export function DataProvider({ children }) {
-  const [dataState, dispatch] = useReducer(dataReducer, { data: [] })
+  useEffect(() => {
+    let subscribed = true;
+    sanityClient
+      .fetch(`*[_type == "Bolum" ]{bolum_no, title, "birim_title": related_birim->title, "birim_no": related_birim-> birim_no, "birim_icon": related_birim->birim_icon, }`)
+      .then((myData) => {
+        if (subscribed) {
+          setBirimler(myData.sort((a, b) => a.birim_no - b.birim_no));
+          console.log("birimler");
+        }
+      })
+      .catch(console.error);
+    console.log("birimler out");
+    return () => {
+      subscribed = false;
+    };
+  }, []);
   return (
-    <DataContext.Provider value={dataContext}>
+    <DataContext.Provider value={birimler}>
       <DataDispatchContext.Provider value={dispatch}>
         {children}
       </DataDispatchContext.Provider>
@@ -30,3 +49,6 @@ export function DataProvider({ children }) {
   )
 }
 
+
+
+const initialData = {} 
