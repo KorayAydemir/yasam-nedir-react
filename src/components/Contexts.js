9192c524dc19773e6fed1c423d0da9f5edb3878e
Koray Aydemir <korayaydemir276@gmail.com>
2022-10-20 00:25:58 +0300

    a

--- src/components/Contexts.js
+++ src/components/Contexts.js
@@ -1,47 +1,28 @@
 import { useState, createContext, useEffect, useReducer } from 'react';
 import sanityClient from "../Client"
 
-export const DataContext = createContext(null)
+export const DataContext = createContext()
 export const DataDispatchContext = createContext(null);
 
 
-export function DataProvider({ children }) {
-  const [data, dispatch] = useReducer(dataReducer, initialData)
-  const [birimler, setBirimler] = useState(null)
-  function dataReducer(data, action) {
-    switch (action.type) {
-      case 'birim-bolum': {
-        return ["a"]
-      }
-      case 'alt-bolum': {
-        return action
-      }
-      case 'denemeler': {
-        return { obj: "abc" }
-      }
-      default: {
-        throw Error('Unknown action: ' + action.type);
-      }
+function dataReducer(state, action) {
+  switch (action.type) {
+    case 'birim-bolum': {
+      return ["a"]
+    }
+    case 'alt-bolum': {
+      return action
+    }
+    default: {
+      throw Error('Unknown action: ' + action.type);
     }
   }
-  useEffect(() => {
-    let subscribed = true;
-    sanityClient
-      .fetch(`*[_type == "Bolum" ]{bolum_no, title, "birim_title": related_birim->title, "birim_no": related_birim-> birim_no, "birim_icon": related_birim->birim_icon, }`)
-      .then((myData) => {
-        if (subscribed) {
-          setBirimler(myData.sort((a, b) => a.birim_no - b.birim_no));
-          console.log("birimler");
-        }
-      })
-      .catch(console.error);
-    console.log("birimler out");
-    return () => {
-      subscribed = false;
-    };
-  }, []);
+}
+
+export function DataProvider({ children }) {
+  const [dataState, dispatch] = useReducer(dataReducer, { data: [] })
   return (
-    <DataContext.Provider value={birimler}>
+    <DataContext.Provider value={dataContext}>
       <DataDispatchContext.Provider value={dispatch}>
         {children}
       </DataDispatchContext.Provider>
@@ -49,6 +30,3 @@ export function DataProvider({ children }) {
   )
 }
 
-
-
-const initialData = {} 
--- src/components/DenemelerMain/DenemelerMain.js
+++ src/components/DenemelerMain/DenemelerMain.js
@@ -1,18 +1,25 @@
 import classes from "./DenemelerMain.module.css"
 import { useOutletContext } from "react-router-dom"
 import { DataDispatchContext, DataContext } from "../Contexts"
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
+import sanityClient from "../../Client"
 
 const DenemelerMain = () => {
-
-  const dispatch = useContext(DataDispatchContext)
   const context = useContext(DataContext)
-  useEffect(() => {
-  })
   const { setTitle } = useOutletContext();
+  const [data, setData] = useState(null)
   //  console.log(context)
   setTitle("Test")
-  console.log(context)
+  useEffect(() => {
+    let subbed = true
+    sanityClient
+      .fetch(`*[_type == "Bolum" ]{bolum_no, title, "birim_title": related_birim->title, "birim_no": related_birim-> birim_no, "birim_icon": related_birim->birim_icon, }`)
+      .then((data) => { if (subbed) { console.log("ran"); setData(data) } })
+    return () => {
+      subbed = false
+    }
+  }, [])
+  console.log(context.data)
   return (
     <div className="site-container">
       <div className={classes.wrapper}>
